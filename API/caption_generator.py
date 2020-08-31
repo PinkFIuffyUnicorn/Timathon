aeef9417db1d87581e73ee9da911eef0fe248550
I-EAT-DATA <54181160+I-EAT-DATA@users.noreply.github.com>
2020-08-31 09:58:34 -0500

    Update caption_generator.py

--- API/caption_generator.py
+++ API/caption_generator.py
@@ -3,23 +3,14 @@ from pickle import load
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 from keras.models import load_model
-from nltk.translate.bleu_score import corpus_bleu
 from keras.applications.vgg16 import VGG16
 from keras.models import Model
 from keras.preprocessing.image import load_img
 from keras.preprocessing.image import img_to_array
 from keras.applications.vgg16 import preprocess_input
 
-root_dir = "N:/captionGen/"
-
 # extract the feature from the photo
-def extract_feature(filename):
-    # load the model
-    model = VGG16()
-
-    # re-structure the model
-    model.layers.pop()
-    model = Model(inputs=model.inputs, outputs=model.layers[-1].output)
+def extract_feature(model, filename):
 
     loaded_image = load_img(filename, target_size=(224, 224))
 
@@ -53,26 +44,33 @@ def generate_desc(model, tokenizer, photo):
 	for i in range(34):
 		# integer encode input sequence
 		sequence = tokenizer.texts_to_sequences([in_text])[0]
+		
 		# pad input
 		sequence = pad_sequences([sequence], maxlen=34)
+
 		# predict next word
 		yhat = model.predict([photo,sequence], verbose=0)
+
 		# convert probability to integer
 		yhat = argmax(yhat)
+
 		# map integer to word
 		word = word_for_id(yhat, tokenizer)
+
 		# stop if we cannot map the word
 		if word is None:
 			break
+		
 		# append as input for generating the next word
 		in_text += ' ' + word
+
 		# stop if we predict the end of the sequence
 		if word == 'endseq':
 			break
 	return in_text
 	
-def generate_caption(model, tokenizer, photo_filename):
-	photo = extract_feature(photo_filename)
+def generate_caption(VGG16_model, model, tokenizer, photo_filename):
+	photo = extract_feature(VGG16_model, photo_filename)
 
 	yhat = generate_desc(model, tokenizer, photo)
 
@@ -85,10 +83,10 @@ def generate_caption(model, tokenizer, photo_filename):
 
 def init_generator():
 	# load the model
-	model_file = 'model-ep006-loss3.232-val_loss3.753.h5'
+	model_file = 'final_model.h5'
 	model = load_model(model_file)
 	
 	# prepare tokenizer
-	tokenizer = load(open(root_dir + 'tokenizer.pkl', 'rb'))
+	tokenizer = load(open('tokenizer.pkl', 'rb'))
 
 	return model, tokenizer
